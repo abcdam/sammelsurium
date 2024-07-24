1a526bc0a4453d73c3a6d68a43b6f77fb01ddb47
abcdam <lukasada@students.zhaw.ch>
2024-07-24 05:47:36 +0200

    make lib file available

---
+++ routines/bash-lib.sh
@@ -0,0 +1,18 @@
+is_root {
+    if [ "$EUID" -eq 0 ]; then
+        return 0  # true
+    fi
+    return 1  # false
+}
+
+function throw {
+    printf '%s\n' "$1" >&2 ## Send message to stderr.
+    exit "${2-1}" ## Return a code specified by $2, or 1 by default.
+}
+
+function file_exists {
+  if [ -f "$1" ]; then
+      return 0
+  fi
+  return 1
+}
