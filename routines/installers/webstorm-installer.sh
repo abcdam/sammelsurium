24d5ced67849259d1f75135132ba8a8c53104264
abcdam <lukasada@students.zhaw.ch>
2024-07-29 07:51:24 +0200

    simple webstorm IDE installer

---
+++ routines/installers/webstorm-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/dash
+USERLIB=/home/$(whoami)/lib
+USERBIN=/home/$(whoami)/bin
+NAME="WebStorm-2024.1.5"
+WBSLIB="$USERLIB/$NAME"
+cd /tmp/
+wget https://download.jetbrains.com/webstorm/$NAME.tar.gz
+
+mkdir -p "$WBSLIB" 
+tar -xzvf "$NAME.tar.gz" --strip-components=1 -C "$WBSLIB"
+ln -s "$WBSLIB/bin/webstorm.sh" $USERBIN/wbs
+
+output=$(type wbs)
+[ -f "$output" ] && echo "webstorm can be started using 'wbs' command". && exit 0
+echo "Add $USERBIN to PATH variable to make 'wbs' (webstorm launcher) callable everywhere"
