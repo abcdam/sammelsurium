d8b3edd1cbd678af00524dff71421cbf13233ec9
abcdam <lukasada@students.zhaw.ch>
2024-07-29 07:55:31 +0200

    retrieve symlink path fix

--- routines/installers/webstorm-installer.sh
+++ routines/installers/webstorm-installer.sh
@@ -10,6 +10,6 @@ mkdir -p "$WBSLIB"
 tar -xzvf "$NAME.tar.gz" --strip-components=1 -C "$WBSLIB"
 ln -s "$WBSLIB/bin/webstorm.sh" $USERBIN/wbs
 
-output=$(type wbs)
+output=$(which wbs)
 [ -f "$output" ] && echo "webstorm can be started using 'wbs' command". && exit 0
 echo "Add $USERBIN to PATH variable to make 'wbs' (webstorm launcher) callable everywhere"
