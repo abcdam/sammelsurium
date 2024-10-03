32094f8f0c5023fefcef07455434a42716144e37
abcdam <lukasada@students.zhaw.ch>
2024-10-03 09:59:46 +0200

    add py libs for algebra/crypto and term plotting

--- routines/docker/python/pytainer.sh
+++ routines/docker/python/pytainer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Wrapper around docker to run python scripts in isolated environment.
 #   Accepts path to py script + args if available
+. /usr/lib/helper-func.sh
 IMG=python:devenv
 script=$(realpath $1)
 shift
@@ -8,4 +9,7 @@ shift
 
 script_dir=$(dirname $script)
 script_id=$(basename $script)
-docker run --rm -v "$script_dir:/app" $IMG python /app/$script_id "$@"
+# forward terminal size for adaptive plotext rendering
+COLUMNS=$(tput cols) LINES=$(tput lines) docker run --rm \
+    -e COLUMNS -e LINES \
+    -v "$script_dir:/app" $IMG python /app/$script_id "$@"
