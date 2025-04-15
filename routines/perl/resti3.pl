618cb2cbc13603aa0da8cc2ac7e125a283fc1a2b
abcdam <gh@abcdam.ch>
2025-04-15 10:00:28 +0200

    cleaned up layout restorer structure

--- routines/perl/resti3.pl
+++ routines/perl/resti3.pl
@@ -11,124 +11,171 @@ use File::Basename;
 use File::Path qw(make_path);
 use IPC::Run qw(run);
 
-my $CACHE_PATH     = '~/.cache/i3/resti3_history.json';
-my $CACHE          = setup_cache($CACHE_PATH);
-my %active_wspaces = get_workspaces();
+my($CACHE_PATH)          = glob '~/.cache/i3/resti3_history.json';
+my $CACHE                = setup_cache($CACHE_PATH);
+my %AVAILABLE_WORKSPACES = get_available_workspaces();
+my $USAGE
+  = sprintf 'Usage: %s [-w <available workspace id>] [-c <layout config path>]'
+  , basename $0;
 ### end section CONFIG
 
 ###
 ## section MAIN
 #
-my $config = handle_args();
-my @cmd    = (
-    "i3-msg", "workspace $config->{workspace}; append_layout $config->{config}"
-);
-if (run \@cmd) {
-    $CACHE->{ $config->{tstamp} } = { %{$config}{qw(config workspace)} };
-    open(my $fh, '>', $CACHE_PATH)
-      or die sprintf "Could not open cache file @ '%s': $!"
-      , $CACHE_PATH;
-    say $fh encode_json($CACHE);
-    close($fh);
-} else {
-    die sprintf "Failed to load layout configuration using cmd: '%s'"
-      , join ' ', @cmd;
-}
+my %opt;
+GetOptions(
+    'w=i' => sub {
+        my($flag_id, $workspace_id) = @_;
+        die "error: flag -w value must be an integer between 1 and 9.\n"
+          if $workspace_id < 1 || $workspace_id > 9;
+        die "error: workspace id '$workspace_id' already in use\n"
+          if ! $AVAILABLE_WORKSPACES{$workspace_id};
+        $opt{workspace} = $workspace_id;
+    },
+    'c=s' => sub {
+        my($flag_id, $layout_config_path) = @_;
+        my $err_prefix
+          = "error: Config path passed with -c flag '$layout_config_path'";
+        die "$err_prefix does not exist\n"
+          unless -e $layout_config_path;
+        die "$err_prefix must be a .json file\n"
+          unless $layout_config_path =~ /\.json$/;
+        $opt{layout_filep} = abs_path($layout_config_path);
+    },
+) or die "$USAGE\n";
+
+my $config = handle_args(\%opt);
+my $cmd    = [
+    "i3-msg",
+    "workspace $config->{workspace}; append_layout $config->{layout_filep}"
+];
+run $cmd, '>', '/dev/null'
+  or die sprintf "Layout restoration failed for cmd '%s'. Caught: '%s'\n"
+  , join ' ', @{$cmd}
+  , $!;
+update_cache($config);
 ### end section MAIN
 
 
 ###
 ## section FUN
 #
-sub get_workspaces {
-    my @cmd = ("i3-msg", "-t", "get_workspaces");
-    my $output;
-
-    die "Failed to get current i3 workspaces: $?"
-      unless run \@cmd, '>', \$output;
-    return map {$_->{num} => $_} @{ decode_json($output) };
-}
-
-
 sub setup_cache {
     my($cache_path) = @_;
     my $loaded_cache = {};
-    say($cache_path);
-    if (! -e $cache_path) {
-        make_path(dirname($cache_path));
-    } else {
+    if (-e $cache_path) {
         open(my $fh, '<', $cache_path)
           or die "Could not open cache file '$cache_path' $!";
         local $/;
         $loaded_cache = decode_json(<$fh>);
         close($fh);
+    } else {
+        make_path(dirname($cache_path));
     }
     return $loaded_cache;
 }
 
 
+sub get_available_workspaces {
+    my $cmd = [ "i3-msg", "-t", "get_workspaces" ];
+    my $output;
+    die "Failed to get current i3 workspaces: $!"
+      unless run $cmd, '>', \$output;
+
+    my %available = map {
+        $_ => 1
+    } 1 .. 9;
+    delete $available{ $_->{num} } for @{ decode_json($output) };
+    die "All workspaces in use.\n"
+      unless keys %available;
+    return %available;
+}
+
+
 sub handle_args {
-    my($workspace_id, $config_path);
-    my $config;    # = { workspace => '', config => '', tstamp = '' };
-    GetOptions(
-        'w=i' => \$workspace_id,
-        'c=s' => \$config_path,
-    ) or die("Error in command line arguments\n");
+    my($workspace, $layout_filep) = @{ shift() }{qw(workspace layout_filep)};
 
-    if (defined $workspace_id) {
-        die "Option -w must be an integer between 1 and 9.\n"
-          if $workspace_id < 1 || $workspace_id > 9;
-        die "workspace '$workspace_id' already in use"
-          if $active_wspaces{$workspace_id};
-        $config->{workspace} = $workspace_id;
-    }
-
-    if (defined $config_path) {
-        die "Option -c arg '$config_path' does not exist or is not a json\n"
-          unless -e $config_path && $config_path =~ /\.json$/;
-        $config->{config} = abs_path($config_path);
-        if (! $config->{workspace}) {
-            print "Available workspaces: ";
-            print join ', '
-              , grep {defined} map {$_ unless $active_wspaces{$_}} 1 .. 9;
-            say "select an available worskpace number:";
-            chomp(my $select = <STDIN>);
-            die "Invalid selection '$select'.\n"
-              unless $select =~ /^\d+$/
-              && $select >= 1
-              && $select <= @$CACHE
-              && ! $active_wspaces{$select};
-            $config->{workspace} = $select;
+    my $config = {};   # = { workspace => '', layout_filep => '', tstamp = '' };
+    if (! $layout_filep) {
+        my $entry_id = prompt_valid_cache_selection($CACHE);
+        $config->{layout_filep} = $CACHE->{$entry_id}{layout_filep};
+        if (! $workspace) {
+            $config->{workspace} = $CACHE->{$entry_id}{workspace};
+            $config->{tstamp}    = $entry_id;
+        } else {
+            delete $CACHE->{$entry_id};
         }}
-    elsif (! $config->{workspace}) {
-        die "Cannot select from empty cache" if keys %$CACHE == 0;
-        my @stamps  = sort {$a <=> $b} keys %$CACHE;
-        my $padding = join '', (' ') x 14;
-        my $sep     = join '', ('-') x 56;
-        printf "%3s %20s %20s\n", 'ID', 'WORKSPACE_NUM', 'CONFIG_PATH';
-        say $sep;
-        for (my $i = 1; $i <= @stamps; $i++) {
-            printf "%3s %14s %s %s\n"
-              , $i
-              , $CACHE->{ $stamps[ $i - 1 ] }{workspace}
-              , $padding
-              , $CACHE->{ $stamps[ $i - 1 ] }{config};
-        }
-        say $sep;
-        print "select a cached config by [ID]: ";
-        chomp(my $select = <STDIN>);
-        die "Invalid selection '$select'.\n"
-          unless $select =~ /^\d+$/
-          && $select >= 1
-          && $select <= @stamps;
-        $config = {
-            config    => $CACHE->{ $stamps[ $select - 1 ] }{config},
-            workspace => $CACHE->{ $stamps[ $select - 1 ] }{workspace},
-            tstamp    => $stamps[ $select - 1 ]
-        };
-    } ## end elsif (! $config->{workspace...})
-    $config->{tstamp} = time unless $config->{tstamp};
-    return $config;
+    elsif (! $workspace) {
+        $config->{workspace} = prompt_valid_workplace_selection();
+    }
+    $config->{layout_filep} //= $layout_filep;
+    $config->{workspace}    //= $workspace;
+    $config->{tstamp}       //= time;
 
+    return $config;
 } ## end sub handle_args
+
+
+sub update_cache {
+    my( $ws,
+        $layout_config_filepath,
+        $uid_timestamp
+    ) = @{ shift() }{qw(workspace layout_filep tstamp)};
+    $CACHE->{$uid_timestamp} = {
+        layout_filep => $layout_config_filepath,
+        workspace    => $ws
+    };
+    open(my $fh, '>', $CACHE_PATH)
+      or die sprintf "Could not open cache file @ '%s': %s"
+      , $CACHE_PATH
+      , $!;
+    say $fh encode_json($CACHE);
+    close($fh);
+}
+
+
+sub prompt_valid_cache_selection {
+    my($cache) = @_;
+    my @timestamps = sort keys %$cache;
+    my($sep, $pad) = ('-' x 80, ' ' x 8);
+
+    die "Cache is empty\n" unless @timestamps > 0;
+    printf "%3s %12s %19s\n", 'ID', 'Workspace', 'Location Config';
+    say $sep;
+    for (0 .. $#timestamps) {
+        my $entry = $cache->{ $timestamps[$_] };
+        printf "%3d %7d %s %s\n"
+          , $_ + 1
+          , $entry->{workspace}
+          , $pad
+          , $entry->{layout_filep};
+    }
+    say $sep;
+
+    printf 'Select config [1-%s]: '
+      , scalar @timestamps;
+
+    chomp(my $input = <STDIN>);
+
+    die "Invalid selection '$input'\n"
+      unless $input =~ /^\d+$/
+      && $input > 0
+      && $input <= @timestamps;
+
+    return $timestamps[ $input - 1 ];
+} ## end sub prompt_valid_cache_selection
+
+
+sub prompt_valid_workplace_selection {
+    my @ws_ids = sort keys %AVAILABLE_WORKSPACES;
+    say sprintf 'Available workspaces: %s'
+      , join ', ', @ws_ids;
+    print 'Set new workspace: ';
+
+    chomp(my $input = <STDIN>);
+
+    die "Invalid selection '$input'.\n"
+      unless $AVAILABLE_WORKSPACES{$input};
+    return $input;
+}
 ### end section FUN
