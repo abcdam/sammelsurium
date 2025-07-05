87e80aff9c964c8b8970cd7ef996989af94c76d9
abcdam <gh@abcdam.ch>
2025-07-06 00:04:31 +0200

    posher test update

--- routines/shell/lib/posher.d/color-util.sh
+++ routines/shell/lib/posher.d/color-util.sh
@@ -1,46 +1,78 @@
 
-_parse_hue_opt() {
-    case "${1:?"_parse_hue_opt requires param to be set"}" in
-        n)      printf '0';;
-        b)      printf '1';;
-        f)      printf '2';;
-        i)      printf '3';;
-        u)      printf '4';;
-        *)      printf '0';;
-    esac
+
+_parse_ANSI_style_opt() {
+    ANSI_style_opt=${1:-}
+    is_param_set "$ANSI_style_opt" && retval=$? || retval=$?
+
+    if [ $retval -ne 0 ]; then
+        fmt="$__POSHER_ERR_MSG_PREFIX _parse_hue_opt(): requires param to be set"
+        printf "$fmt\n" >&2
+    else
+        case "$ANSI_style_opt" in
+            n|normal)       printf '0';;
+            b|bold)         printf '1';;
+            f|faint)        printf '2';;
+            i|italic)       printf '3';;
+            u|underline)    printf '4';;
+            *) retval=64
+        fmt="$__POSHER_ERR_MSG_PREFIX: %s. (expected: one of '%s' - got: '%s')"
+                printf "$fmt\n"                                 \
+                    "_parse_hue_opt(): invalid font modifier"   \
+                    "bfuni"                                     \
+                    "$ANSI_style_opt" >&2
+                ;;
+        esac
+    fi
+    unset ANSI_style_opt fmt
+    return $retval
 }
 
-_parse_hue_color() {
-    case "$1" in
-        ""|w|white)             printf '37';;
-        r|red)                  printf '31';;
-        g|green)                printf '32';;
-        y|yellow)               printf '33';;
-        b|blue)                 printf '34';;
-        m|magenta|p|purple)     printf '35';;
-        c|cyan|t|turquoise)     printf '36';;
-        gray)                   printf '90';;
-        lr|lightred|lred)       printf '91';;
-        lg|lightgreen|lgreen)   printf '92';;
-        ly|lightyellow|lyellow) printf '93';;
-        lb|lightblue|lblue)     printf '94';;
-        lm|lightmagenta|lmagenta|lp|lpurple|lightpurple) printf '95';;
-        lc|lightcyan|lcyan|lt|lturquoise|lightturquoise) printf '96';;
-        black)                  printf '30';;
-        tw|twhite|truewhite)    printf '97';;
-        *)                      printf '37';;
-    esac
+_parse_ANSI_color() {
+    ANSI_color_opt=${1:-}
+    is_param_set "$ANSI_color_opt" && retval=$? || retval=$?
+
+    if [ $retval -ne 0 ]; then
+        fmt="$__POSHER_ERR_MSG_PREFIX _parse_ANSI_color(): requires param to be set"
+        printf "$fmt\n" >&2
+    else
+        case "$ANSI_color_opt" in
+            w|white)                printf '37';;
+            r|red)                  printf '31';;
+            g|green)                printf '32';;
+            y|yellow)               printf '33';;
+            b|blue)                 printf '34';;
+            m|magenta|p|purple)     printf '35';;
+            c|cyan)                 printf '36';;
+            gray)                   printf '90';;
+            lr|lightred )           printf '91';;
+            lg|lightgreen)          printf '92';;
+            ly|lightyellow)         printf '93';;
+            lb|lightblue)           printf '94';;
+            lm|lightmagenta|lp|lightpurple) printf '95';;
+            lc|lightcyan)           printf '96';;
+            black)                  printf '30';;
+            tw|truewhite)           printf '97';;
+            *)  retval=64
+        fmt="$__POSHER_ERR_MSG_PREFIX: %s. (expected: valid 4-bit color key - got: '%s')"
+                printf "$fmt\n"                                 \
+                    "_parse_ANSI_color(): invalid color option" \
+                    "$ANSI_color_opt" >&2
+                ;;
+        esac
+    fi
+    unset ANSI_color_opt fmt
+    return $retval
 }
 
 # Output: cosmetically modified input string according to given params
 # $1: txt (required): string of chars to be colored
-# _color (default (37) white): supports basic 4-bit color set -> check switch case for keys
-# _option (default (n)ormal): (n)ormal, (b)old, (f)aint, (i)talics, (u)nderlined
+# $2: _color (default (37) white): supports basic 4-bit color set -> check switch case for keys
+# $3: _option (default (n)ormal): (n)ormal, (b)old, (f)aint, (i)talic, (u)nderline
 #
 hue() {
     [ -z "$1" ] && printf "Error: missing 1st positional argument (target text) in hue() params.\n" >&2 && exit 1
-    _hue_color="$(_parse_hue_color "${2:-}")"
-    _hue_opt="$(_parse_hue_opt "${3:-}")"
+    _hue_color="$(_parse_ANSI_color "${2:-}")"
+    _hue_opt="$(_parse_ANSI_style_opt "${3:-}")"
     RESET='\033[0m'
     printf "\033[${_hue_opt};${_hue_color}m${1}${RESET}"
 }
--- routines/shell/lib/posher.t/tests/color-util.test.sh
+++ routines/shell/lib/posher.t/tests/color-util.test.sh
@@ -2,12 +2,12 @@ load_posher color
 # set -x
 assert_equal() {
     got_ov="$1"
-    got_ec="$2"
     exp_ov_pattern="$3"
     exp_ec="${4:-0}"
-    src_id="$5"
+    src_id="${5:?'assert_equal(): no test id given'}"
+    got_ec="${2:?"assert_equal(): requires exit code of executed cmd in test '$src_id'"}"
 
-    excode_info="error code: got: $got_ec, expected: $exp_ec)"
+    excode_info="(Error Code: expected/$exp_ec - got/$got_ec)"
     diff_substr_expansion=${got_ov#*$exp_ov_pattern}
     if [ $got_ec -eq $exp_ec ] && [ "$diff_substr_expansion" != "$got_ov" ]; then
         printf '%s\n' "PASS: $src_id"
@@ -16,34 +16,130 @@ assert_equal() {
     else
       retval=1
 printf '%s\n' '--------------------------------------------------------------------------------'
-printf "\033[0;31mFAIL\033[0m: %s\n" "$src_id"
-printf "Expected: " >&2; printf '%s' "$exp_ov_pattern" | od -t x1 >&2
-printf "Actual  : " >&2; printf '%s' "$got_ov" | od -t x1 >&2
-printf '%s\n' "$excode_info" >&2
+printf "\033[0;31mFAIL\033[0m: test id: '%s'\n" "$src_id"         >&2
+printf "Expected: '%s'\n"                       "$exp_ov_pattern" >&2
+printf "Actual  : '%s'\n"                       "$got_ov"         >&2
+printf '%s\n'                                   "$excode_info"    >&2
 printf '%s\n' '--------------------------------------------------------------------------------'
     fi
     exit 1
-    
 }
-####################
-###  Test Cases    #
-####################
-test_parse_hue_opt() {
 
-# Each line is: <test input> | <expected exit code> | 
-#               <expected output pattern/substring> | <test id>
-  cat <<'EOF' | while IFS='|' read -r input exp_excode exp_pattern test_id; do
-|2|_parse_hue_opt requires param to be set|no option
-n|0|0|(n)ormal option
-b|0|1|(b)old option
-f|0|2|(f)aint option
-i|0|3|(i)talics option
-u|0|4|(u)nderline option
-invalid|0|0|non-existent option
-EOF
+run_tests() {
+  parser="${1:?'run_tests(): missing parser'}"; shift
 
-got_outval=$( _parse_hue_opt "$input" 2>&1 ) && got_excode=$? || got_excode=$?
-    assert_equal "$got_outval" "$got_excode" "$exp_pattern" "$exp_excode" "$test_id"
-    done
-    unset IFS got_outval got_excode exp_pattern exp_excode test_id
+  grep -v -E '^\s*(#|$)'  \
+    | while IFS='|'       \
+        read -r test_id input exp_pattern exp_excode; do
+        got_outval=$("$parser" "$input" 2>&1) && got_excode=$? || got_excode=$?
+        assert_equal "$got_outval" "$got_excode" "$exp_pattern" "$exp_excode" "$test_id"
+      done
 }
+
+
+####################
+###  Test Cases  ###
+####################
+test_parse_ANSI_style_opt() {
+
+  run_tests '_parse_ANSI_style_opt' <<'TESTS'
+#####
+####
+### TABLE ROW DEF:
+##   < test id >|<test input>|< expected output pattern/substring >|< expected exit code >
+#
+
+
+# expected errors
+no ANSI_style option||_parse_hue_opt(): requires param to be set|64
+non-existent ANSI_style option|stereospace|(expected: one of 'bfuni' - got: 'stereospace')|64
+
+# happy paths
+(n)ormal ANSI_style option|n|0|0
+normal ANSI_style option|normal|0|0
+
+(b)old ANSI_style option|b|1|0
+bold ANSI_style option|bold|1|0
+
+(f)aint ANSI_style option|f|2|0
+faint ANSI_style option|faint|2|0
+
+(i)talic ANSI_style option|i|3|0
+italic ANSI_style option|italic|3|0
+
+(u)nderline ANSI_style option|u|4|0
+underline ANSI_style option|underline|4|0
+
+TESTS
+} ## END test_parse_ANSI_style_opt()
+
+
+
+test_parse_ANSI_color() {
+
+  run_tests '_parse_ANSI_color' <<'TESTS'
+#####
+####
+### TABLE ROW DEF:
+##   < test id >|<test input>|< expected output pattern/substring >|< expected exit code >
+#
+
+
+# expected errors
+no ANSI_color option||_parse_ANSI_color(): requires param to be set|64
+non-existent ANSI_color option|ultraviolent|(expected: valid 4-bit color key - got: 'ultraviolent')|64
+
+(w)hite ANSI_color option|w|37|0
+white ANSI_color option|white|37|0
+
+(r)ed ANSI_color option|r|31|0
+red ANSI_color option|red|31|0
+
+(g)reen ANSI_color option|g|32|0
+green ANSI_color option|green|32|0
+
+(y)ellow ANSI_color option|y|33|0
+yellow ANSI_color option|yellow|33|0
+
+(b)lue ANSI_color option|b|34|0
+blue ANSI_color option|blue|34|0
+
+(m)agenta ANSI_color option|m|35|0
+magenta ANSI_color option|magenta|35|0
+(p)urple ANSI_color option|p|35|0
+purple ANSI_color option|purple|35|0
+
+(c)yan ANSI_color option|c|36|0
+cyan ANSI_color option|cyan|36|0
+
+gray ANSI_color option|gray|90|0
+
+(l)ight(r)red  ANSI_color option|lr|91|0
+lightred  ANSI_color option|lightred|91|0
+
+
+(l)ight(g)reen ANSI_color option|lg|92|0
+lightgreen ANSI_color option|lightgreen|92|0
+
+
+(l)ight(y)ellow ANSI_color option|ly|93|0
+lightyellow ANSI_color option|lightyellow|93|0
+
+(l)ight(b)lue ANSI_color option|lb|94|0
+lightblue ANSI_color option|lightblue|94|0
+
+(l)ight(m)agenta ANSI_color option|lm|95|0
+lightmagenta ANSI_color option|lightmagenta|95|0
+(l)ight(p)urple ANSI_color option|lp|95|0
+lightpurple ANSI_color option|lightpurple|95|0
+
+(l)ight(c)yan ANSI_color option|lc|96|0
+lightcyan ANSI_color option|lightcyan|96|0
+
+black ANSI_color option|black|30|0
+
+(t)rue(w)hite ANSI_color option|tw|97|0
+truewhite ANSI_color option|truewhite|97
+
+TESTS
+} ## END test_parse_ANSI_color()
